4f102fc36fe8b68e8e6829bb847a9d28d29de4df
Syed Mudhasir Mahmood <m1068767@mindtree.com>
2023-05-02 10:39:07 +0530

    final

--- src/app.js
+++ src/app.js
@@ -4,12 +4,22 @@ const express = require('express');
 
 const app = express();
 
+const { accounts, users, writeJSON } = require('./data.js');
+
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes = require('./routes/services.js')
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
